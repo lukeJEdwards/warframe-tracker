d88265a9ada1e1bb8434a74f5bbc967b6e8b00bd
Luke Edwards <lukeedwards30167116@gmail.com>
2024-07-28 19:53:01 +0100

    routes setup

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import './assets/main.css'
+import "./scss/base.scss"
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { createRouter, createWebHistory } from "vue-router";
+import { routes } from "vue-router/auto-routes";
+
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
 app.use(createPinia())
 app.use(router)
--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,42 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import VueRouter from "unplugin-vue-router/vite";
+import { imagetools } from "vite-imagetools";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter({
+      // how and what folders to scan for files
+      routesFolder: [
+        {
+          src: "src/routes",
+          path: "",
+          // override globals
+          exclude: (excluded) => excluded,
+          filePatterns: (filePatterns) => filePatterns,
+          extensions: (extensions) => extensions,
+        },
+      ],
+
+      extensions: [".vue"],
+      filePatterns: ["**/*"],
+      exclude: [],
+      dts: "./typed-router.d.ts",
+      importMode: "async",
+      root: process.cwd(),
+      pathParser: {
+        dotNesting: true,
+      },
+    }),
     vue(),
     vueDevTools(),
+    imagetools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
